011b198f6bafaf2a0418dd3dc7e1ec8ac77f034f
Justin-del <beibei031119@gmail.com>
2025-05-01 18:55:43 +0800

    commit

--- Database/Sessions/Sessions.go
+++ Database/Sessions/Sessions.go
@@ -4,9 +4,13 @@ import (
 	"crypto/rand"
 	"database/sql"
 	"encoding/hex"
+	"strconv"
 
 	_ "github.com/mattn/go-sqlite3"
 )
+
+//The idle timeout is common to be 15 to 30 minutes for low risk applications and 2-5 minutes for high risk applications. See https://cheatsheetseries.owasp.org/cheatsheets/Session_Management_Cheat_Sheet.html#session-expiration 
+var idleTimeOutInMinutes int = 15
 
 func CreateTableIfNotExists() {
 	database, _ := sql.Open("sqlite3", "file:Database.sqlite")
@@ -14,7 +18,7 @@ func CreateTableIfNotExists() {
 
 }
 
-func CreateASession(user_id string) string {
+func CreateASession(userId string) string {
 	database, _ := sql.Open("sqlite3", "file:Database.sqlite")
 	id := make([]byte, 32)
 
@@ -22,26 +26,28 @@ func CreateASession(user_id string) string {
 
 	encoded_id := hex.EncodeToString(id)
 
-	//The idle timeout is common to be 15 to 30 minutes for low risk applications and 2-5 minutes for high risk applications. See https://cheatsheetseries.owasp.org/cheatsheets/Session_Management_Cheat_Sheet.html#session-expiration Also, you might want to lower the absolute timeout for high risk applications.
-	database.Exec("insert into sessions (id,absolute_time_out, idle_time_out, user_id) values (?,datetime('now','+24 hour'), datetime('now','+15 minute'),?)", encoded_id, user_id)
+	//You might want to lower the absolute timeout for high risk applications.
+	database.Exec("insert into sessions (id,absolute_time_out, idle_time_out, user_id) values (?,datetime('now','+24 hour'), datetime('now',?),?)", encoded_id, "+"+strconv.Itoa(idleTimeOutInMinutes)+" minutes", userId)
 
 	return encoded_id
 }
 
-func DoesSessionExistsInDatabase(session_id string) bool {
+func DoesSessionExistsInDatabase(sessionId string) bool {
 	database, _ := sql.Open("sqlite3", "file:Database.sqlite")
 
 	//Before that, delete all sessions that are expired.
 	database.Exec("delete from sessions where datetime('now')>absolute_time_out OR datetime('now')>idle_time_out")
 
-	var result bool
-	database.QueryRow("select exists(select 1 FROM sessions where id=?)", session_id).Scan(&result)
+	var returnedSessionId string
 
-	return result
+	//Extend the session by the defined idle timeout in minutes.
+	database.QueryRow("update sessions set idle_time_out=datetime('now',?) where id=? RETURNING id", "+"+strconv.Itoa(idleTimeOutInMinutes)+" minutes", sessionId).Scan(&returnedSessionId)
+
+	return returnedSessionId != ""
 }
 
-func DeleteSession(session_id string) {
+func DeleteSession(sessionId string) {
 	database, _ := sql.Open("sqlite3", "file:Database.sqlite")
 
-	database.Exec("delete from sessions where id=?", session_id)
+	database.Exec("delete from sessions where id=?", sessionId)
 }
